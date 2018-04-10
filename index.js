182fc757583ed061fc8b3b656a7931f5101b17c2
wangzheng <wangzheng@shimo.im>
2018-04-10 23:50:55 +0800

    feat: upload json and search it

--- index.js
+++ index.js
@@ -3,15 +3,39 @@ const app = new Koa()
 const Router = require('koa-router')
 const router = new Router()
 const fs = require('fs')
+const multer = require('koa-multer')
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => cb(null, './data/'),
+    filename: (req, file, cb) => {
+      cb(null, file.originalname)
+    }
+  })
+})
 
 router.get('/', (ctx) => {
   ctx.type = 'text/html'
   ctx.body = fs.createReadStream('./views/index.html')
 })
-router.get('/ai', async (ctx) => {
+router.get('/search/:tag', async (ctx) => {
+  const tag = ctx.params.tag
   ctx.type = 'application/json'
-  const data = fs.readFileSync('./data/AI.json')
+  const data = fs.readFileSync(`./data/${tag}.json`)
   ctx.body = data
+})
+router.get('/upload', ctx => {
+  ctx.type = 'text/html'
+  ctx.body = fs.createReadStream('./views/upload.html')
+})
+router.post('/upload', upload.single('upload'), async (ctx) => {
+  const originName = ctx.req.file.originalName
+  if (originName === 'AI.json') {
+    ctx.assert(403, '已有该文件')
+  }
+  ctx.body = {
+    code: 0,
+    data: null
+  }
 })
 app.use(router.routes())
 app.listen(3000)
